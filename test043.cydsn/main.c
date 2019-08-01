6122baf722d6ab6868bc65aa2abff21ed71ca79c
dogtopus <dogtopus@users.noreply.github.com>
2019-08-01 14:07:18 -0300

    Enable tuner

--- test043.cydsn/main.c
+++ test043.cydsn/main.c
@@ -9,6 +9,7 @@
 #include "project.h"
 #include <stdbool.h>
 
+#define TUNER
 //#define BIT_CTL_SCAN_EN 1
 #define BIT_CTL_INTR_EN 1 << 1
 #define BIT_CTL_INTR_TRIG 1 << 7
@@ -54,6 +55,7 @@ typedef struct {
     slider_protocol_ro_t ro;
 } __attribute__((packed)) slider_protocol_t;
 
+#ifndef TUNER
 volatile slider_protocol_t i2cregs = {0};
 
 const uint32_t SEG_MAPPING[] = {
@@ -91,10 +93,7 @@ const uint32_t SEG_MAPPING[] = {
     Slider_SEGMENTS_SNS31_ID,
 };
 
-int main(void) {
-    /* Enable global interrupts. */
-    CyGlobalIntEnable;
-
+void setup() {
     /* Setup */
     // Set protocol version
     i2cregs.ro.ver_major = 1;
@@ -114,54 +113,84 @@ int main(void) {
     // Initialize CapSense
     Slider_Start();
     Slider_ScanAllWidgets();
+}
+
+void loop() {
+    // Assert interrupt pin if necessary
+    Pin_Interrupt_Out_Write((i2cregs.rw.ctl & BIT_CTL_INTR_TRIG) ? PIN_LOW : PIN_HIZ);
+    if (Slider_IsBusy() == Slider_NOT_BUSY) {
+        // Apply advanced filters
+        Slider_ProcessAllWidgets();
+        bool dirty = false;
+        // Check for all individual sensors
+        if (Slider_IsWidgetActive(Slider_SEGMENTS_WDGT_ID)) {
+            Pin_Status_LED_Write(PIN_HIGH);
+            for (uint8_t i=0; i<(sizeof(SEG_MAPPING) / sizeof(uint32_t)); i++) {
+                uint8_t bit = (Slider_IsSensorActive(Slider_SEGMENTS_WDGT_ID, SEG_MAPPING[i]) ? 1 : 0);
+                uint8_t state_bit_pos = i & 7;
+                volatile uint8_t *reg = &i2cregs.ro.keys[i >> 3];
+                uint8_t bit_orig = ((*reg >> state_bit_pos) & 1);
+                if (bit_orig != bit) {
+                    dirty = true;
+                    // Flip the bit
+                    *reg ^= 1 << state_bit_pos;
+                    if (bit) {
+                        LED_Pixel(i, 0, LED_CURSOR);
+                    } else {
+                        LED_Pixel(i, 0, LED_BG);
+                    }
+                }
+            }
+        // No sensor is active, check if any of them were active
+        } else if (i2cregs.ro.keys_active) {
+            Pin_Status_LED_Write(PIN_LOW);
+            // Nothing active, zeroing
+            i2cregs.ro.keys_active = 0;
+            // Clear LED
+            LED_DisplayClear(LED_BG);
+            dirty = true;
+        }
+        // Re-arm the widget scanner
+        Slider_ScanAllWidgets();
+
+        // Update the LED
+        LED_Trigger(1);
+
+        // Sync the interrupt bit with dirty bit 
+        if (dirty && (i2cregs.rw.ctl & BIT_CTL_INTR_EN)) {
+            i2cregs.rw.ctl |= BIT_CTL_INTR_TRIG;
+        }
+    }   
+}
+#else
+void setup() {
+    // Initialize I2C
+    I2C_Start();
+    I2C_EzI2CSetBuffer1(sizeof(Slider_dsRam), sizeof(Slider_dsRam), (uint8_t *) &Slider_dsRam);
+
+    // Initialize CapSense
+    Slider_Start();
+    Slider_ScanAllWidgets();
+}
+
+void loop() {
+    if (Slider_IsBusy() == Slider_NOT_BUSY) {
+        Slider_ProcessAllWidgets();
+        Slider_RunTuner();
+        Slider_ScanAllWidgets();
+    }
+}
+#endif
+
+int main(void) {
+    /* Enable global interrupts. */
+    CyGlobalIntEnable;
+
+    setup();
 
     /* Main loop */
     for(;;) {
-        // Assert interrupt pin if necessary
-        Pin_Interrupt_Out_Write((i2cregs.rw.ctl & BIT_CTL_INTR_TRIG) ? PIN_LOW : PIN_HIZ);
-        if (Slider_IsBusy() == Slider_NOT_BUSY) {
-            // Apply advanced filters
-            Slider_ProcessAllWidgets();
-            bool dirty = false;
-            // Check for all individual sensors
-            if (Slider_IsWidgetActive(Slider_SEGMENTS_WDGT_ID)) {
-                Pin_Status_LED_Write(PIN_HIGH);
-                for (uint8_t i=0; i<(sizeof(SEG_MAPPING) / sizeof(uint32_t)); i++) {
-                    uint8_t bit = (Slider_IsSensorActive(Slider_SEGMENTS_WDGT_ID, SEG_MAPPING[i]) ? 1 : 0);
-                    uint8_t state_bit_pos = i & 7;
-                    volatile uint8_t *reg = &i2cregs.ro.keys[i >> 3];
-                    uint8_t bit_orig = ((*reg >> state_bit_pos) & 1);
-                    if (bit_orig != bit) {
-                        dirty = true;
-                        // Flip the bit
-                        *reg ^= 1 << state_bit_pos;
-                        if (bit) {
-                            LED_Pixel(i, 0, LED_CURSOR);
-                        } else {
-                            LED_Pixel(i, 0, LED_BG);
-                        }
-                    }
-                }
-            // No sensor is active, check if any of them were active
-            } else if (i2cregs.ro.keys_active) {
-                Pin_Status_LED_Write(PIN_LOW);
-                // Nothing active, zeroing
-                i2cregs.ro.keys_active = 0;
-                // Clear LED
-                LED_DisplayClear(LED_BG);
-                dirty = true;
-            }
-            // Re-arm the widget scanner
-            Slider_ScanAllWidgets();
-
-            // Update the LED
-            LED_Trigger(1);
-
-            // Sync the interrupt bit with dirty bit 
-            if (dirty && (i2cregs.rw.ctl & BIT_CTL_INTR_EN)) {
-                i2cregs.rw.ctl |= BIT_CTL_INTR_TRIG;
-            }
-        }
+        loop();
     }
     /* Should never jump to here */
     return 0;
