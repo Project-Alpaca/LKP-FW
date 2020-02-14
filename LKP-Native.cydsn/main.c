39cdfcc3d752116b784935177e4ed8d183b6dfaa
dogtopus <dogtopus@users.noreply.github.com>
2020-02-14 15:06:15 -0400

    Linear interpolation for non-pixel-perfect LED strips

--- LKP-Native.cydsn/main.c
+++ LKP-Native.cydsn/main.c
@@ -21,6 +21,9 @@
 #define LED_BG 0x121212
 #define LED_CURSOR 0xffffff
 
+#define NUM_PHYSICAL_LEDS LED_COLUMNS
+#define NUM_EFFECTIVE_PIXELS 32u
+
 typedef struct {
     /* control and configuration regs - writable */
     // 0: scan_en (currently disabled) 1: interrupt_en 7: interrupt_trig
@@ -100,6 +103,77 @@ const uint32_t SEG_MAPPING[] = {
     Slider_SEGMENTS_SNS31_ID,
 };
 
+static void clearLED(uint32_t color);
+static void setLEDWithInterpol(uint32 offset, uint32_t color);
+static void updateLEDWithInterpol();
+
+#if (NUM_EFFECTIVE_PIXELS == NUM_PHYSICAL_LEDS)
+    // TODO
+    static void clearLED(uint32_t color) {
+        LED_DisplayClear(color);
+    }
+    static void setLEDWithInterpol(uint32 offset, uint32_t color) {
+        LED_Pixel((int32_t) offset, 0, color);
+    }
+    static void updateLEDWithInterpol() {
+        LED_Trigger(1);
+    }
+#else
+    #define _MAX_PIXEL (NUM_EFFECTIVE_PIXELS - 1)
+    #define _MAX_LED (NUM_PHYSICAL_LEDS - 1)
+    #define _LED_COMMON_DENOM (_MAX_PIXEL * _MAX_LED)
+    uint32_t _effective_pixels[NUM_EFFECTIVE_PIXELS] = {0};
+    static void clearLED(uint32_t color) {
+        for (uint32_t i=0; i<NUM_EFFECTIVE_PIXELS; i++) {
+            _effective_pixels[i] = color;
+        }
+    }
+    static void setLEDWithInterpol(uint32 offset, uint32_t color) {
+        _effective_pixels[offset] = color;
+    }
+    static void updateLEDWithInterpol() {
+        // Loop through all physical LEDs
+        for (uint32_t led_idx=0; led_idx<NUM_PHYSICAL_LEDS; led_idx++) {
+            // Rebase the physical LED # to common denominator
+            uint32_t int_pos = led_idx * _LED_COMMON_DENOM / _MAX_LED;
+            // Determine the interval
+            uint32_t pixel0 = int_pos * _MAX_PIXEL / _LED_COMMON_DENOM;
+            uint32_t pixel1 = pixel0 + 1;
+            // Rebase the (now integer) interval to the common denominator
+            uint32_t pixel0_pos = pixel0 * _LED_COMMON_DENOM / _MAX_PIXEL;
+            uint32_t pixel1_pos = pixel1 * _LED_COMMON_DENOM / _MAX_PIXEL;
+            if (pixel1 >= NUM_EFFECTIVE_PIXELS) {
+                // Copy the pixel if we are at the 1.0 case
+                LED_Pixel(_MAX_LED, 0, _effective_pixels[_MAX_PIXEL]);
+            } else {
+                // Actually do the interpolation
+                uint32_t pixel_interpol = 0;
+                // Copy pixel0 and pixel1 for easier manipulation
+                uint32_t pixel0_data = _effective_pixels[pixel0];
+                uint32_t pixel1_data = _effective_pixels[pixel1];
+                uint8_t bitpos = 0;
+                while (pixel0_data != 0 || pixel1_data != 0) {
+                    // Read color
+                    uint32_t color0 = pixel0_data & 0xff;
+                    uint32_t color1 = pixel1_data & 0xff;
+                    // Do the interpolation
+                    uint32_t color_interpol;
+                    color_interpol = (color0 * (pixel1_pos - int_pos) + color1 * (int_pos - pixel0_pos)) / (pixel1_pos - pixel0_pos);
+                    // Set the interpolated value
+                    pixel_interpol |= ((color_interpol & 0xff) << bitpos);
+                    // Update bitpos and shift out the processed byte
+                    pixel0_data >>= 8;
+                    pixel1_data >>= 8;
+                    bitpos += 8;
+                }
+                // Copy to LED display buffer
+                LED_Pixel((int32_t) led_idx, 0, pixel_interpol);
+            }
+        }
+        LED_Trigger(1);
+    }
+#endif
+
 void setup() {
     /* Setup */
     // Initialize I2C
@@ -110,8 +184,9 @@ void setup() {
     LED_Start();
     // Hard code the dim level to 1/2 for now
     LED_Dim(1);
-    LED_DisplayClear(LED_BG);
-    LED_Trigger(1);
+    // Initial LED states
+    clearLED(LED_BG);
+    updateLEDWithInterpol();
 
     // Initialize CapSense
     Slider_Start();
@@ -138,9 +213,9 @@ void loop() {
                     // Flip the bit
                     *reg ^= 1 << state_bit_pos;
                     if (bit) {
-                        LED_Pixel(i, 0, LED_CURSOR);
+                        setLEDWithInterpol(i, LED_CURSOR);
                     } else {
-                        LED_Pixel(i, 0, LED_BG);
+                        setLEDWithInterpol(i, LED_BG);
                     }
                 }
             }
@@ -150,14 +225,14 @@ void loop() {
             // Nothing active, zeroing
             i2cregs.ro.keys_active = 0;
             // Clear LED
-            LED_DisplayClear(LED_BG);
+            clearLED(LED_BG);
             dirty = true;
         }
         // Re-arm the widget scanner
         Slider_ScanAllWidgets();
 
         // Update the LED
-        LED_Trigger(1);
+        updateLEDWithInterpol();
 
         // Sync the interrupt bit with dirty bit 
         if (dirty && (i2cregs.rw.ctl & BIT_CTL_INTR_EN)) {
