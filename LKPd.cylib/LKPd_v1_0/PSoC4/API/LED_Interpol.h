90c1f587a987bcbcb8bdf212a37c3fa94bdb0a81
dogtopus <dogtopus@users.noreply.github.com>
2021-04-09 21:43:39 -0300

    LKPd: Add extern to LED_Interpol

--- LKPd.cylib/LKPd_v1_0/PSoC4/API/LED_Interpol.h
+++ LKPd.cylib/LKPd_v1_0/PSoC4/API/LED_Interpol.h
@@ -14,8 +14,8 @@
 #define `$INSTANCE_NAME`_NUM_PHYSICAL_LEDS `$INSTANCE_NAME`_IO_LED_COLUMNS
 #define `$INSTANCE_NAME`_NUM_EFFECTIVE_PIXELS 32u
 
-void `$INSTANCE_NAME`_ClearLED(uint32_t color);
-void `$INSTANCE_NAME`_SetLED(uint32_t offset, uint32_t color);
-void `$INSTANCE_NAME`_CommitLED();
+extern void `$INSTANCE_NAME`_ClearLED(uint32_t color);
+extern void `$INSTANCE_NAME`_SetLED(uint32_t offset, uint32_t color);
+extern void `$INSTANCE_NAME`_CommitLED();
 
 /* [] END OF FILE */
